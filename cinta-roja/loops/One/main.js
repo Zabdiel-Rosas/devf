9547fea7964cc143ee39462de62c8e78d6371569
Zabdiel <zabdiel_rosas@outlook.com>
2020-02-18 17:52:03 -0600

    Se agrega la primera parte de los ejercicios de loops

---
+++ cinta-roja/loops/One/main.js
@@ -0,0 +1,24 @@
+//First Exercise
+// for(let i = 1; i <= 1000; i++) {
+//     console.log(i);
+// }
+
+//Second Exercise
+// for(let i = 1000; i >= 1; i--) {
+//     console.log(i);
+// }
+
+//Third Exercise
+// for(let i = 1; i <= 10000; i++) {
+//     console.log('Este es el número ' + i);
+// }
+
+//Fourth Exercise
+// let sum = 0;
+// for(let i=0; i <= 10; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+//Fith Exercise
+
