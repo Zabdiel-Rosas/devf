159d80a3cfbd5c1db0dec293765672c1ea37b9c1
Zabdiel <zabdiel_rosas@outlook.com>
2020-02-21 14:18:03 -0600

    Se sube nuevo ejercicio

---
+++ cintaRoja38/Herencia/jsAsync/main.js
@@ -0,0 +1,10 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+console.log(1);
+// callback, cuantoTiempoVaAEstarEnLaPilaEnMS
+setTimeout(function(){
+    console.log(2);
+} ,10000);
+console.log(3);
